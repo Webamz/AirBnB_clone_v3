081535a09552db4360b7d0a23b70f34ba8cdc9ab
Wanjiru393 <carolciru393@gmail.com>
2023-07-31 22:45:15 +0300

    routing

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-"""Init file in the views module
-"""
 from flask import Blueprint
 
-
+# Create the app_views variable as an instance of Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
+# Import the route from index.py to add it to the app_views blueprint
+from api.v1.views.index import status
+
